5bd627736f9ee62312ba11388b34debef0951c90
joelh790 <joel.hartney@gmail.com>
2016-11-12 17:12:37 +0000

    Comments on the payment class

--- CS4227Project/src/payment/Payment.java
+++ CS4227Project/src/payment/Payment.java
@@ -24,18 +24,18 @@ public class Payment {
 		String username = arr[0];
 		String mediaTitle = arr[1];
 		String purchaseType = arr[2];
-		String paymentOption = arr[3];
-		I_Receipt receipt= new ReceiptA();
+		String paymentOption = arr[3];                
+		I_Receipt receipt= new ReceiptA();	//receipt object created
 		
 		UserClass customer= database.getUserByName(username);
 		int userID = customer.getUserID();
-		CustomerClass cust= (CustomerClass)customer;
+		CustomerClass cust= (CustomerClass)customer; //Casting from Userclass to CustomerClass
 		MediaItem item= database.getMediaItemByName(mediaTitle);
 		double oldBalance= Double.parseDouble(cust.getBalance());
 		double price= item.getPrice();
 		double newBalance= oldBalance-price;
 			
-		if((newBalance>=0.0 && paymentOption.equalsIgnoreCase("Wallet")) || paymentOption.equalsIgnoreCase("Credit Card") )
+		if((newBalance>=0.0 && paymentOption.equalsIgnoreCase("Wallet")) || paymentOption.equalsIgnoreCase("Credit Card") ) //both cases mean their payment is valid
 		{
 			// ENOUGH MONEY!
 		
@@ -54,13 +54,13 @@ public class Payment {
 					logPaymentSuccessOrFailure(Integer.toString(userID), message, false);
 					return;
 				}
-				receipt= new CustomerDecorator(new OnlineDecorator(receipt));
+				receipt= new CustomerDecorator(new OnlineDecorator(receipt)); //receipt decorated so that a customer has bought an item to be stored in online repository
 				
 			}
 			
 			else if(purchaseType.equalsIgnoreCase("Ship to Address"))
 			{
-				receipt= new CustomerDecorator(new ShipmentDecorator(receipt));
+				receipt= new CustomerDecorator(new ShipmentDecorator(receipt)); //receipt decorated so that a customer has bought an item to be shipped to them
 			}
 			
 			if(paymentOption.equalsIgnoreCase("Wallet"))
@@ -68,43 +68,42 @@ public class Payment {
 			// MAKES PAYMENT : UPDATES CUSTOMER BALANCE
 
 			String updatedBalance= Double.toString(newBalance);
-			cust.setBalance(updatedBalance);
+			cust.setBalance(updatedBalance);  //updating customer wallet
 			String message = "Successful payment : Paid: " + price + " for " + mediaTitle;
-			logPaymentSuccessOrFailure(Integer.toString(userID), message, true);
+			logPaymentSuccessOrFailure(Integer.toString(userID), message, true); //interceptor method??
 			
 
-				updatedBalance= Double.toString(newBalance);
-				cust.setBalance(updatedBalance);
-				receipt= new WalletDecorator(receipt);
+				
+				receipt= new WalletDecorator(receipt); //receipt decorated to show that the customer paid by wallet
 			}
 			
 			if(paymentOption.equalsIgnoreCase("Credit Card"))
 			{	
-				receipt= new CreditCardDecorator(receipt);
+				receipt= new CreditCardDecorator(receipt); //receipt decorated to show that the customer paid by credit card
 			}
 			
 
-			userInterface.UserInterfaceMenu execut = new UserInterfaceMenu();
-			execut.displayReceipt(item,receipt);
+			userInterface.UserInterfaceMenu execut = new UserInterfaceMenu(); //call to user interface
+			execut.displayReceipt(item,receipt); //displayReceipt method of UI is called which implements the PrintReceipt() method and prints out all the decorators that are dynamically bound to the Receipt
 		
 				
 				// THIS UPDATES USERS.TXT WITH THE NEW BALANCE FOR CUSTOMER
-			database.updateUsers();
+			database.updateUsers(); //refresh users so that their new wallet balance is updated to the text file
 		}
 		else
 		{
 			String message = ("Payment Cancelled! : Insufficient Funds!");
 			userInterface.UserInterfaceMenu execute = new UserInterfaceMenu();
 			execute.displayErrorMessage(message);
-			logPaymentSuccessOrFailure(Integer.toString(userID), message, false);
+			logPaymentSuccessOrFailure(Integer.toString(userID), message, false); //interceptor??
 			return;
 		}
 		
 		if(item.getMediaType().equalsIgnoreCase("GAME"))
 		{
-			GameClass itemG= (GameClass)item;
+			GameClass itemG= (GameClass)item; 
 			PlatformChoice pChoice= new PlatformChoice();
-			pChoice.nullPLatform(itemG);
+			pChoice.nullPLatform(itemG); //When a platform is chosen for a game item the price is changed depending on the platform, nullPlatform is called to reset the game item back to its original price
 		}
 	}
 	
